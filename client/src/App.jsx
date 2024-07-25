0304d4b41314dc689f046cda7dbf2bf66a75ea55
JPhanGT3 <148407787+JPhanGT3@users.noreply.github.com>
2024-07-24 20:40:46 -0400

    Add color and padding to search button icon

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,10 +14,10 @@ function App() {
         <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">  
           <div>Anywhere</div>
           <div className="border-l border-gray-300"> </div>
-          <div>Any week</div>
+          <div>Any week</div >
           <div className="border-l border-gray-300"> </div>
           <div>Add guests</div>
-          <button>
+          <button className="bg-primary text-white p-2 rounded-full">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
           </svg>
