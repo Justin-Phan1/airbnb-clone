e5c0659abf66d8daebb85fb173f728a8e124cebd
JPhanGT3 <148407787+JPhanGT3@users.noreply.github.com>
2024-07-24 20:13:04 -0400

    Uploading all files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,13 @@ function App() {
 
   return (
     <div>
-      <header> 
-        <a href ="" className="logo>"> </a>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+      <header className="p-4"> 
+        <a href ="" className="flex items-center gap-1>">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 -rotate-90">
           <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
         </svg>
-
+        <span className="font-bold text-xl"> airbnb</span>
+        </a>
       </header>
     </div>
   )
