e70377e806398285109776fb19ab2cc64eac98ca
JPhanGT3 <148407787+JPhanGT3@users.noreply.github.com>
2024-07-26 23:37:45 -0400

    Add input fields for email and password and button to login

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,3 +1,11 @@
 export default function LoginPage(){
-    return 'login form should go here';
+    return (
+        <div>
+            <form>
+                <input type="email" placeholder={'your@email.com'}/>
+                <input type="password" placeholder="password" />
+                <button>Login</button>
+            </form>
+        </div>
+    );
 }
